16665de53349bfd5e1b20132c7b9e299ea982413
Jakub Karch <jakkar120@gmail.com>
2019-10-23 13:21:41 +0200

    Ćwiczenia z platformy - iteracje i warunki

---
+++ PPK - pętle i warunki/Ćwiczenie 1/Cwiczenie_1/Cwiczenie_1.cpp
@@ -0,0 +1,26 @@
+﻿// Cwiczenie_1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int W=5, K=8;
+
+	for (int i = 0; i < W; i++)
+	{
+		for (int j = 0; j < K; j++)
+		{
+			if (i % 2 == 0)
+				cout << "X";
+			else
+				cout << ".";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
---
+++ PPK - pętle i warunki/Ćwiczenie 5/Cwiczenie_5/Cwiczenie_5.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int N = 7;
+	int pom = N;
+
+	for (int i = 0; i < N / 2; i++)
+	{
+		for (int k = 0; k < (N - pom) / 2; k++)
+			cout << ".";
+		for (int j = 0; j < pom; j++)
+			cout << "X";
+		for (int k = 0; k < (N - pom) / 2; k++)
+			cout << ".";
+		cout << endl;
+		pom -= 2;
+
+	}
+
+	for (int i = 0; i < N / 2 + 1; i++)
+	{
+		for (int k = 0; k < (N - pom) / 2; k++)
+			cout << ".";
+		for (int j = 0; j < pom; j++)
+			cout << "X";
+		for (int k = 0; k < (N - pom) / 2; k++)
+			cout << ".";
+		cout << endl;
+		pom += 2;
+	}
+
+	return 0;
+}
